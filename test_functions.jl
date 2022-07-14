b195b450537e6835381ef5b1c18deb0711616dd6
Personal laptop <f.wguerra@outlook.com>
2022-07-14 21:12:05 +0200

    first test function

---
+++ test_functions.jl
@@ -0,0 +1,17 @@
+x = zeros(6,1000000)
+
+function g(x)
+    n_dims = size(x)[2]
+    #println(n_dims)
+
+    a = 20.0
+    b = 0.2
+    c = 2 * pi
+
+    auxiliar = -b .* sqrt.((sum(x.^2, dims=2)./ n_dims))
+    auxiliar_2 = sum(cos.(c .* x), dims = 2) ./ n_dims
+    g = - a .* exp.(auxiliar) .- exp.(auxiliar_2) .+ a .+ exp(1.0)
+
+end
+
+g(x)
